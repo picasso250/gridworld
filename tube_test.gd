df88d5938b3752a66979b2e9284dbadebaefbe02
pica <xi_aochi@163.com>
2024-11-25 16:59:56 +0800

    test tube

---
+++ tube_test.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@export var grid_size: Vector2i = Vector2i(4, 4)  # n x m grid size
+@export var cell_size: Vector2 = Vector2(64, 64)  # Each cell size
+
+var template_tube = preload("res://tube.tscn").instantiate()
+
+# Stores the tube grid
+var tubes = []
+
+func _ready():
+	# Initialize grid data
+	for y in range(grid_size.y):
+		var row = []
+		for x in range(grid_size.x):
+			# Use the tube template to initialize each grid element
+			var tube = template_tube.duplicate() 
+
+			# Set the position and scale of the tube
+			tube.position = Vector2(x, y) * cell_size
+			tube.scale = cell_size / Vector2(128, 128)  # Adjust scale based on grid size
+			tube.visible = true
+			tube.x = y
+			tube.y = x
+			add_child(tube)
+
+			# Add the newly created tube to the tubes grid
+			row.append(tube)
+
+		tubes.append(row)
