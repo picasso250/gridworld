e02646219efd1152de6a67429c21883a154968ed
pica <xi_aochi@163.com>
2024-11-26 12:02:45 +0800

    水开始移动

--- tube.gd
+++ tube.gd
@@ -14,34 +14,44 @@ extends Node2D
 # 参数化颜色
 @export var pipe_color: Color = Color(0.545, 0.271, 0.075, 1) # 管壁颜色 (土黄色)
 @export var water_color: Color = Color(0, 0, 1, 1) # 水的颜色 (蓝色)
+var water_size = pipe_width - pipe_thickness * 2 # 水的大小
+
+var water_pos: Vector2 = Vector2(-water_size / 2, -water_size / 2)  # 水的初始位置
+var water_target: Vector2 = Vector2(32, 0) # 水的目标位置
+var speed: float = 50 # 水流的速度，每秒移动的像素数
 
 func _draw():
-    var water_size = pipe_width - pipe_thickness * 2 # 水的大小
 
-    if draw_left_pipe:
-        # 绘制左半边的管道
-        draw_rect(Rect2(Vector2(-cell_width / 2, -pipe_width / 2), Vector2(cell_width / 2 + pipe_width / 2, pipe_width)), pipe_color)
+	if draw_left_pipe:
+		# 绘制左半边的管道
+		draw_rect(Rect2(Vector2(-cell_width / 2, -pipe_width / 2), Vector2(cell_width / 2 + pipe_width / 2, pipe_width)), pipe_color)
 
-    if draw_right_pipe:
-        # 绘制右半边的管道
-        draw_rect(Rect2(Vector2(-pipe_width / 2, -pipe_width / 2), Vector2(cell_width / 2 + pipe_width / 2, pipe_width)), pipe_color)
+	if draw_right_pipe:
+		# 绘制右半边的管道
+		draw_rect(Rect2(Vector2(-pipe_width / 2, -pipe_width / 2), Vector2(cell_width / 2 + pipe_width / 2, pipe_width)), pipe_color)
 
-    if draw_top_pipe:
-        # 绘制上半边的管道
-        draw_rect(Rect2(Vector2(-pipe_width / 2, -cell_width / 2), Vector2(pipe_width, cell_width / 2 + pipe_width / 2)), pipe_color)
+	if draw_top_pipe:
+		# 绘制上半边的管道
+		draw_rect(Rect2(Vector2(-pipe_width / 2, -cell_width / 2), Vector2(pipe_width, cell_width / 2 + pipe_width / 2)), pipe_color)
 
-    if draw_bottom_pipe:
-        # 绘制下半边的管道
-        draw_rect(Rect2(Vector2(-pipe_width / 2, -pipe_width / 2), Vector2(pipe_width, cell_width / 2 + pipe_width / 2)), pipe_color)
+	if draw_bottom_pipe:
+		# 绘制下半边的管道
+		draw_rect(Rect2(Vector2(-pipe_width / 2, -pipe_width / 2), Vector2(pipe_width, cell_width / 2 + pipe_width / 2)), pipe_color)
 
-    # 绘制水：考虑管道壁的厚度
-    draw_rect(Rect2(Vector2(-water_size / 2, -water_size / 2), Vector2(water_size, water_size)), water_color)
+	# 绘制水：考虑管道壁的厚度
+	draw_rect(Rect2(water_pos, Vector2(water_size, water_size)), water_color)
 
-    if debug:
-        # 绘制调试十字
-        var cross_color: Color = Color(0.8, 0.8, 0.8, 0.1) # 淡灰色
-        draw_rect(Rect2(Vector2(-cell_width / 2, -pipe_width / 2), Vector2(cell_width, pipe_width)), cross_color)
-        draw_rect(Rect2(Vector2(-pipe_width / 2, -cell_width / 2), Vector2(pipe_width, cell_width)), cross_color)
+	if debug:
+		# 绘制调试十字
+		var cross_color: Color = Color(0.8, 0.8, 0.8, 0.1) # 淡灰色
+		draw_rect(Rect2(Vector2(-cell_width / 2, -pipe_width / 2), Vector2(cell_width, pipe_width)), cross_color)
+		draw_rect(Rect2(Vector2(-pipe_width / 2, -cell_width / 2), Vector2(pipe_width, cell_width)), cross_color)
 
-func _ready():
-    pass
+func _process(delta):
+	# 水的移动：每一帧根据速度更新水的水平位置
+	var direction = Vector2(1,0)
+	water_pos += direction * speed * delta
+	queue_redraw()
+
+
+	
