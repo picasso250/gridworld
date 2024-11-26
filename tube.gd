898ff7352f77b1bbdd3c101752cc69ea8b22bee4
pica <xi_aochi@163.com>
2024-11-26 11:36:04 +0800

    左半边的管道

--- tube.gd
+++ tube.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 @export var x = 0
 @export var y = 0
-@export var cell_width:float = 64 # 格子宽度
-@export var pipe_width:float = 32 # 水管直径
-@export var pipe_thickness:float = 4 # 水管壁厚度
-@export var is_horizontal = true # 是否为水平管道
+@export var cell_width: float = 64 # 格子宽度
+@export var pipe_width: float = 32 # 水管直径
+@export var pipe_thickness: float = 4 # 水管壁厚度
+@export var draw_left_pipe: bool = true # 是否画左半边的管道
+@export var draw_right_pipe: bool = true # 是否画右半边的管道
 @export var debug = false # 是否开启调试模式
 
 # 参数化颜色
@@ -13,22 +14,22 @@ extends Node2D
 @export var water_color = Color(0, 0, 1, 1) # 水的颜色 (蓝色)
 
 func _draw():
-	var water_size = pipe_width-pipe_thickness*2
-	if is_horizontal:
-		# 将矩形的中心点对齐到 (0, 0)
-		draw_rect(Rect2(Vector2(-cell_width / 2, -pipe_width / 2), Vector2(cell_width, pipe_width)), pipe_color)
-	else:
-		# 将竖直管壁的中心点对齐到 (0, 0)
-		draw_rect(Rect2(Vector2(-pipe_width / 2, -cell_width / 2), Vector2(pipe_width, cell_width)), pipe_color)
+	var water_size = pipe_width - pipe_thickness * 2 # 水的大小
 
+	if draw_left_pipe:
+		# 绘制左半边的管道
+		draw_rect(Rect2(Vector2(-cell_width / 2, -pipe_width / 2), Vector2(cell_width / 2+pipe_width/2, pipe_width)), pipe_color)
 
-	# 绘制水
+	if draw_right_pipe:
+		# 绘制右半边的管道
+		draw_rect(Rect2(Vector2(-pipe_width / 2, -pipe_width / 2), Vector2(cell_width / 2+pipe_width / 2, pipe_width)), pipe_color)
+
+	# 绘制水：考虑管道壁的厚度
 	draw_rect(Rect2(Vector2(-water_size / 2, -water_size / 2), Vector2(water_size, water_size)), water_color)
-
 
 	if debug:
 		# 绘制调试十字
-		var cross_color = Color(0.8, 0.8, 0.8, 0.5) # 淡灰色
+		var cross_color = Color(0.8, 0.8, 0.8, 0.1) # 淡灰色
 		draw_rect(Rect2(Vector2(-cell_width / 2, -pipe_width / 2), Vector2(cell_width, pipe_width)), cross_color)
 		draw_rect(Rect2(Vector2(-pipe_width / 2, -cell_width / 2), Vector2(pipe_width, cell_width)), cross_color)
 
